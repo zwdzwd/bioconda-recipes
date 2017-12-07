948c96049f4089a3d6e4e8a44157e799291d7a1b
caleb <caleb.easterly@gmail.com>
2017-12-07 15:40:20 -0600

    fix conf/algorithm-properties.txt not found error

--- recipes/mpa-portable/build.sh
+++ recipes/mpa-portable/build.sh
@@ -7,6 +7,8 @@ mkdir -p $PREFIX/bin
 cp -R * $outdir/
 cp $RECIPE_DIR/mpa-portable.py $outdir/mpa-portable
 ln -s $outdir/mpa-portable $PREFIX/bin
+mkdir $PREFIX/bin/conf
+ln -s $outdir/conf/algorithm-properties.txt $PREFIX/bin/conf
 chmod 0755 "${PREFIX}/bin/mpa-portable"
 if [ -f $outdir/built/Comet/linux/comet.exe ] ; then chmod 0755 $outdir/built/Comet/linux/comet.exe ; fi
 if [ -f $outdir/built/X!Tandem/linux/linux_64bit/tandem ] ; then chmod 0755 $outdir/built/X!Tandem/linux/linux_64bit/tandem ; fi
