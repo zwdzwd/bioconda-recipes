d2847dc7edcf89bcdcf449088268ebc5d59b4c6a
zwdzwd <zhouwanding@gmail.com>
2018-03-13 22:35:34 -0400

    more fix to ncurses

--- recipes/biscuit/build.sh
+++ recipes/biscuit/build.sh
@@ -2,9 +2,11 @@
 
 mkdir -p $PREFIX/bin
 
+export CFLAGS="$CFLAGS -ltinfo" 
+
 make
 cp biscuit $PREFIX/bin
-# cp scripts/QC.sh $PREFIX/bin
+cp scripts/QC.sh $PREFIX/bin
 
 chmod a+x $PREFIX/bin/biscuit
-# chmod a+x $PREFIX/bin/QC.sh
+chmod a+x $PREFIX/bin/QC.sh
